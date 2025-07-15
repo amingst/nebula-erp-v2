5c5204c64189cb55a17ddcd67460e4d632837451
amingst <3608359+amingst@users.noreply.github.com>
2025-07-15 19:09:55 -0400

    Remove shell scripts and enable Inventory exports
    Deleted obsolete shell scripts for TypeScript and Inventory generation. Uncommented Inventory module exports in ts-gen/index.ts, indicating case sensitivity issues have been resolved.

--- src/Nebula.Fragments/ts-gen/index.ts
+++ src/Nebula.Fragments/ts-gen/index.ts
@@ -6,25 +6,17 @@ export * from './gen/Protos/Nebula/Services/Fragments/Accounting/AccountingInter
 export * from './gen/Protos/Nebula/Services/Fragments/Accounting/AccountRecord_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/Accounting/JournalEntryRecord_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/Accounting/TransactionRecord_pb';
-
 export * from './gen/Protos/Nebula/Services/Fragments/Authentication/UserInterface_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/Authentication/UserRecord_pb';
-
 export * from './gen/Protos/Nebula/Services/Fragments/HR/EmployeeInterface_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/HR/EmployeeRecord_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/HR/PayrollInterface_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/HR/PayrollRecord_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/HR/TimesheetInterface_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/HR/TimesheetRecord_pb';
-
 export * from './gen/Protos/Nebula/Services/Fragments/Organizations/OrganizationInterface_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/Organizations/OrganizationRecord_pb';
-
 export * from './gen/Protos/Nebula/Services/Fragments/Shared/OrganizationRoleAssignment_pb';
-
-// Note: Inventory exports commented out due to case sensitivity issues
-// Uncomment these once the case sensitivity issues are resolved
-/*
 export * from './gen/Protos/Nebula/Services/Fragments/Inventory/BatchInterface_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/Inventory/BatchRecord_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/Inventory/InventoryInterface_pb';
@@ -38,4 +30,3 @@ export * from './gen/Protos/Nebula/Services/Fragments/Inventory/StockMovementRec
 export * from './gen/Protos/Nebula/Services/Fragments/Inventory/StockRecord_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/Inventory/SupplierInterface_pb';
 export * from './gen/Protos/Nebula/Services/Fragments/Inventory/SupplierRecord_pb';
-*/
